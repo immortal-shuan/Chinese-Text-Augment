7489c23b7efd3ec197443bf5cbbd54145e314ff9
immortal-shuan <76055590+immortal-shuan@users.noreply.github.com>
2023-07-04 16:49:07 +0800

    Update google_translator.py

--- back translate/google_translator.py
+++ back translate/google_translator.py
@@ -1,1 +1,67 @@
+import time
+from googletrans import Translator
+from tqdm import trange, tqdm
+import random
 
+translator = Translator(
+    service_urls=['translate.google.com', 'translate.google.cn']
+)
+
+LANGUAGES = {
+    'af': 'afrikaans', 'sq': 'albanian', 'am': 'amharic', 'ar': 'arabic', 'hy': 'armenian', 'az': 'azerbaijani',
+    'eu': 'basque', 'be': 'belarusian', 'bn': 'bengali', 'bs': 'bosnian', 'bg': 'bulgarian', 'ca': 'catalan',
+    'ceb': 'cebuano', 'ny': 'chichewa', 'zh-cn': 'chinese (simplified)', 'zh-tw': 'chinese (traditional)',
+    'co': 'corsican', 'hr': 'croatian', 'cs': 'czech', 'da': 'danish', 'nl': 'dutch', 'en': 'english',
+    'eo': 'esperanto', 'et': 'estonian', 'tl': 'filipino', 'fi': 'finnish', 'fr': 'french', 'fy': 'frisian',
+    'gl': 'galician', 'ka': 'georgian', 'de': 'german', 'el': 'greek', 'gu': 'gujarati', 'ht': 'haitian creole',
+    'ha': 'hausa', 'haw': 'hawaiian', 'iw': 'hebrew', 'hi': 'hindi', 'hmn': 'hmong', 'hu': 'hungarian',
+    'is': 'icelandic', 'ig': 'igbo', 'id': 'indonesian', 'ga': 'irish', 'it': 'italian', 'ja': 'japanese',
+    'jw': 'javanese', 'kn': 'kannada', 'kk': 'kazakh', 'km': 'khmer', 'ko': 'korean', 'ku': 'kurdish (kurmanji)',
+    'ky': 'kyrgyz', 'lo': 'lao', 'la': 'latin', 'lv': 'latvian', 'lt': 'lithuanian', 'lb': 'luxembourgish',
+    'mk': 'macedonian', 'mg': 'malagasy', 'ms': 'malay', 'ml': 'malayalam', 'mt': 'maltese', 'mi': 'maori',
+    'mr': 'marathi', 'mn': 'mongolian', 'my': 'myanmar (burmese)', 'ne': 'nepali', 'no': 'norwegian', 'ps': 'pashto',
+    'fa': 'persian', 'pl': 'polish', 'pt': 'portuguese', 'pa': 'punjabi', 'ro': 'romanian', 'ru': 'russian',
+    'sm': 'samoan', 'gd': 'scots gaelic', 'sr': 'serbian', 'st': 'sesotho', 'sn': 'shona', 'sd': 'sindhi',
+    'si': 'sinhala', 'sk': 'slovak', 'sl': 'slovenian', 'so': 'somali', 'es': 'spanish', 'su': 'sundanese',
+    'sw': 'swahili', 'sv': 'swedish', 'tg': 'tajik', 'ta': 'tamil', 'te': 'telugu', 'th': 'thai', 'tr': 'turkish',
+    'uk': 'ukrainian', 'ur': 'urdu', 'uz': 'uzbek', 'vi': 'vietnamese', 'cy': 'welsh', 'xh': 'xhosa', 'yi': 'yiddish',
+    'yo': 'yoruba', 'zu': 'zulu', 'fil': 'Filipino', 'he': 'Hebrew'
+}
+
+languages_selected = {'en': 'english'}
+
+bt_lang = list(languages_selected.keys())
+
+def back_translation_aug(text, bt_lang):
+    med_texts = []
+    for i, _ in enumerate(bt_lang):
+        num = 0
+        while num < 20:
+            try:
+                med_text = translator.translate(text, dest=_).text
+            except:
+                med_text = ''
+            if med_text != '':
+                med_texts.append(med_text)
+                break
+            num += 1
+    aug_texts = []
+    for temp_text in med_texts:
+        num = 0
+        while num < 20:
+            try:
+                aug_text = translator.translate(temp_text, dest='zh-cn').text
+            except:
+                aug_text = ''
+            if aug_text != '':
+                aug_texts.append(aug_text)
+                break
+            num += 1
+    return aug_texts
+
+
+
+# aug_texts = back_translation_aug(
+#     '全景网8月21日讯c0600919周日晚间发布公告称，因工作调动原因，王弋先生申请辞去公司董事、技术产品总监的职务。',
+#     bt_lang=['en', 'fr', 'de']
+# )
